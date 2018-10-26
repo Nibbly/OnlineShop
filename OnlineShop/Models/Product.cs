4727ae52ac4936feab34bb43242eea05854e5a02
Nibbly <34423578+Nibbly@users.noreply.github.com>
2018-10-26 18:01:29 +0200

    Initial commit. Adds models. TODO databaseconnection

---
+++ OnlineShop/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    enum ProductType
+    {
+        Electronics = 1,
+        Food = 2,
+        Living = 3
+    }
+
+    class Product
+    {
+        public int ProductID { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ProductType ProductType { get; set; }
+
+    }
+}
---
+++ OnlineShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
